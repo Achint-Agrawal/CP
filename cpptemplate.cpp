5d69b950d98914571d1a25269e4a8e5efcf70ac8
Achint1999 <45819170+Achint1999@users.noreply.github.com>
2021-03-15 23:23:55 +0530

    updated template

--- cpptemplate.cpp
+++ cpptemplate.cpp
@@ -3,15 +3,13 @@
 #define ll long long
 #define fo(i, n) for(int i=0; i<n; i++)
 #define Fo(i, a, b, x) for(int i=a; i<=b; i+=x)
-#define fod(i, n) for(int i=n-1; i>=0; i--)
-#define Fod(i, a, b, x) for(int i=a; i>=b; i-=x)
 #define arrin(a, n) fo(i, n) cin>>a[i]
 #define arrin2(a, n) Fo(i, 1, n, 1) cin>>a[i]
 #define arrout(a, n, s) fo(i, n) cout<<a[i]<<s
 #define arrout2(a, n, s) Fo(i, 1, n, 1) cout<<a[i]<<s
 #define mod 1000000007
-#define PI  3.14159265358979323846
 #define pb push_back
+#define mkp make_pair
 #define deb(a) cout<<#a<<"="<<a<<"\n"
 #define deb2(a,b) cout<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n"
 #define deb3(a,b, c) cout<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n"
@@ -26,10 +24,6 @@ typedef vector<vi> vvi;
 typedef map<int,int> mii;
 typedef set<int> si;
 typedef multiset<int> msi;
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int  uint64;
 
 void test_case(){
     
